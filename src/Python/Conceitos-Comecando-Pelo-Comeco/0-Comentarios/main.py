ae9e81e18b776a1f32719384837457f41ef04a04
André Camargo <andre.camargo@msn.com>
2024-09-06 18:03:06 -0300

    DocString

--- src/Python/Conceitos-Comecando-Pelo-Comeco/0-Comentarios/main.py
+++ src/Python/Conceitos-Comecando-Pelo-Comeco/0-Comentarios/main.py
@@ -1,5 +1,11 @@
 # Permite escrever um comentário em linha
 
 """
-    Permite um comentário em bloco
+    Isso é um DocString
+     
+    Permite um comentário em multilinhas, porém isso não é um comentário o python irá ler 
 """
+
+'''
+    Explicar o código usando aspas simples, que também é um DocString
+'''
